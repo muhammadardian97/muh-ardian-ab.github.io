2c29d3d19e4dbbac407c6722f0869c7701ddcbec
Muhammad Ardian Ananda Basandi <45807540+muhardianab@users.noreply.github.com>
2021-03-01 15:32:37 +0700

    Add files via upload

--- assets/js/main.js
+++ assets/js/main.js
@@ -14,8 +14,8 @@
     new Typed('.typed', {
       strings: typed_strings,
       loop: true,
-      typeSpeed: 100,
-      backSpeed: 50,
+      typeSpeed: 75,
+      backSpeed: 30,
       backDelay: 2000
     });
   }
